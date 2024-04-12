c99594f978a3b6695065469b962a6544ae06adf8
Neroil <edwin.haffner@heig-vd.ch>
2024-04-12 14:29:18 +0200

    Les commentaires :3

--- src/main/java/gre/lab1/groupD/TarjanAlgorithm.java
+++ src/main/java/gre/lab1/groupD/TarjanAlgorithm.java
@@ -4,10 +4,43 @@ import gre.lab1.graph.DirectedGraph;
 import gre.lab1.graph.GraphScc;
 import gre.lab1.graph.SccAlgorithm;
 
+import java.util.Stack;
+
 
 public final class TarjanAlgorithm implements SccAlgorithm {
 
-  private DirectedGraph graph;
+  /**
+   * Structure de données qui nous permet de manipuler plus facilement les différentes données utiles
+   * à l'algorithme de Tarjan.
+   *
+   * @author Edwin Haeffner
+   * @author Arthur Junod
+   */
+  private static class DataTarjan {
+    public Stack<Integer> stack = new Stack<>();  // La pile qui nous permet de garder en mémoire les sommets qui nous restent à classer
+    public int numDfsnum = 0;                     // Variable à incrémenter pour générer l'ordre de visite de chaque sommet
+    public int numCompos = 0;                     // Variable à incrémenter pour générer le numéro de chaque composante fortement connexe
+    public int nbVertices;                        // Nombre de sommets
+
+    // Chaque tableau a la taille du nombre de sommet, de ce fait on peut accéder a la donnée relative au sommet i avec tab[i]
+    public int[] dfsnum;  // Tableau contenant l'ordre de visite de chaque sommet
+    public int[] scc;     // Tableau contenant l'appartenance de chaque sommet à une composante fortement connexe
+    public int[] low;     // Tableau contenant le plus vieil ancêtre accessbile de chaque sommet
+
+    /**
+     * Constructeur
+     * @param graph Le graphe sur lequel nous allons appliquer l'algorithme de Tarjan ce qui nous
+     *              permet de préparer les tableaux et récupérer le nombre de sommets
+     */
+    public DataTarjan(DirectedGraph graph){
+      nbVertices  = graph.getNVertices();
+      dfsnum      = new int[nbVertices];
+      scc         = new int[nbVertices];
+      low         = new int[nbVertices];
+    }
+  }
+
+  private DirectedGraph graph; // Le graphique sur lequel on applique Tarjan
 
   /**
    * Applique l'algorithme de Tarjan sur un graphe et permet de récupérer le résultat
@@ -20,7 +53,7 @@ public final class TarjanAlgorithm implements SccAlgorithm {
 
     this.graph = graph;
 
-    Data data = new Data(graph);
+    DataTarjan data = new DataTarjan(graph);
 
     for(int i = 0; i < data.nbVertices; ++i){
       if(data.scc[i] == 0){
@@ -49,14 +82,17 @@ public final class TarjanAlgorithm implements SccAlgorithm {
       // Mettre à jour la valeur "low" de v si successor à un ancêtre accessible qui est plus vieux (si son low est plus petit)
       if(d.scc[successor] == 0) d.low[v] = Math.min(d.low[successor], d.low[v]);
     }
+    /*
+    Si on a pas trouvé d'ancêtre accessible plus vieux que soit même dans les successeur,
+    on peut créer une composante fortement connexe pour ce sommet.
+    */
     if(d.low[v] == d.dfsnum[v]){
-      d.numCompos++; //Commence à numéroter à partir de 1
+      d.numCompos++; //Commence à numéroter les composantes à partir de 1
       int top;
       do{
-        top = d.stack.pop();
-        d.scc[top] = d.numCompos;
-      } while (top != v);
+        top = d.stack.pop(); // On récupère chaque ancêtre grâce à la pile
+        d.scc[top] = d.numCompos; // On donne à chaque ancêtre récupéré le numéro de la composante
+      } while (top != v); // jusqu'à arriver à v
     }
   }
-
 }
